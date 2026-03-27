295a1a15fde1790380ade55ca04f7e4c50ea20a2
蓬松扁山楂 <auwuzzzz@gmail.com>
2026-03-27 23:26:06 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,7 +12,7 @@ export ALLOW_MISSING_DEPENDENCIES=true
 echo -e "\x1b[96mcezanne: 开始添加OrangeFox Vars...\x1b[m"
 ## 构建信息
 # 设置显示在关于页面里的维护人员名称
-export OF_MAINTAINER=AneglaCool,ymdzq,ZhcnPanda
+export OF_MAINTAINER=AneglaCool,ZakoSaza(LoveVanilla13),ymdzq,ZhcnPanda
 
 ## 添加功能
 # 使用完整版ps命令
@@ -92,12 +92,6 @@ export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 # export OF_PATCH_AVB20=1
 # 关闭修改橙狐启动画面功能，这个功能可能会导致重启无限卡MI
 export OF_NO_SPLASH_CHANGE=1
-# 禁用橙狐内置的magisk菜单
-# export FOX_DELETE_MAGISK_ADDON=1
-# 使用指定的magisk
-export FOX_USE_SPECIFIC_MAGISK_ZIP="$HOME/Magisk.zip"
-# 使用指定的magisk版本号，由于magisk 23+使用了新的包装形式，文件路径改变了，橙狐无法获取正确的版本
-export MAGISK_VER=25.2
 # 当修补recovery/boot镜像时，始终指示magiskboot v24+修补vbmeta标头（自动禁用avb验证？）
 export FOX_PATCH_VBMETA_FLAG=1
 # 修复部分设备或者rom的解密问题
