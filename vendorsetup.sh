7f6f9f23cbd3dd69c9dbdda94ba25a6d7066e384
蓬松扁山楂 <auwuzzzz@gmail.com>
2026-03-27 21:38:48 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,9 +13,6 @@ echo -e "\x1b[96mcezanne: 开始添加OrangeFox Vars...\x1b[m"
 ## 构建信息
 # 设置显示在关于页面里的维护人员名称
 export OF_MAINTAINER=AneglaCool,ymdzq,ZhcnPanda
-# 设置版本号为日期
-//export FOX_VERSION=$(date +%y.%m.%d)
-export FOX_MAINTAINER_PATCH_VERSION="12.1_1"
 
 ## 添加功能
 # 使用完整版ps命令
